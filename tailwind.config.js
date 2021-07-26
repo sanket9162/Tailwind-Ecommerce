f3df533689a1f37ca3e67c02bd2bee1f372de125
sanket gondhali <sanketgondhali55@gmail.com>
2021-07-26 09:33:06 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      orange: {
+        100: "#FEEDD3",
+        200: "#FE7865",
+        300: "#E6604D",
+        400: "#F55C46",
+      },
+      blue: {
+        100: "#F5F8FC",
+        200: "#EFEDFE",
+        300: "#5BD5E6",
+      },
+      green: {
+        100: "#FFFBF8",
+        200: "#E8FBF7",
+        300: "#5BE6B0",
+      },
+      gray: {
+        100: "#ECECEE",
+        200: "#DFDAFE",
+        300: "#83838b",
+      },
+      pink: {
+        100: "#FEDAE8",
+      },
+      black: {
+        100: "#2C2E3F",
+        200: "#141517",
+        300: "#2C2E3F",
+        400: "#1D2539",
+      },
+      yellow: {
+        100: "#F55C46",
+      },
+    },
+    extend: {
+      spacing: {
+        144: "36rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

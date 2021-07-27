1a1e8e98bfe6cc0abb045c0ff9f67e324f5c4926
sanket gondhali <sanketgondhali55@gmail.com>
2021-07-27 17:34:40 +0530

    Update

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ $(document).ready(function () {
     appendDots: ".slider-dots",
   });
 });
+
+const triggers = document.querySelectorAll(".menu-trigger");
+
+console.log(triggers);
+
+for (let i = 0; i < triggers.length; i++) {
+  triggers[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    this.classList.toggle("is-open");
+  });
+}
